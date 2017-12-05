988abdbd94f53699d868893ebcae29aab3adafe4
Snaek7 <emerson_franca77@hotmail.com>
2017-12-05 00:14:08 -0300

    files

---
+++ challenge-05/challenge-05.js
@@ -0,0 +1,125 @@
+/*
+Crie uma variável qualquer, que receba um array com alguns valores aleatórios
+- ao menos 5 - (fica por sua conta os valores do array).
+*/
+
+var conjunto = [1,'',2,true,4,{}];
+
+/*
+Crie uma função que receba um array como parâmetro, e retorne esse array.
+*/
+
+function myFunction(array) {
+    return array;
+}
+
+/*
+Imprima o segundo índice do array retornado pela função criada acima.
+*/
+
+myFunction(conjunto)[1];
+
+/*
+Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
+segundo, um número. A função deve retornar um índice do array que foi passado
+no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
+segundo parâmetro.
+*/
+
+function myFunction2(array, indice) {
+    return indice ? array[indice] : array;
+}
+
+/*
+Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
+*/
+
+var conjunto2 = [1,'aaa',42,true,4,{}];
+
+/*
+Invoque a função criada acima, fazendo-a retornar todos os valores do último
+array criado.
+*/
+
+myFunction2(conjunto2);
+
+/*
+Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
+livro. Dentro dessa função, declare uma variável que recebe um objeto com as
+seguintes características:
+- esse objeto irá receber 3 propriedades, que serão nomes de livros;
+- cada uma dessas propriedades será um novo objeto, que terá outras 3
+propriedades:
+    - `quantidadePaginas` - Number (quantidade de páginas)
+    - `autor` - String
+    - `editora` - String
+- A função deve retornar o objeto referente ao livro passado por parâmetro.
+- Se o parâmetro não for passado, a função deve retornar o objeto com todos
+os livros.
+*/
+function book(nomeLivro) {
+    var obj = {
+        'livro1': {
+            quantidadePaginas: 400,
+            autor: 'José escritor',
+            editora: 'record'
+        },
+        'livro2': {
+            quantidadePaginas: 100,
+            autor: 'Manoel escritor',
+            editora: 'macaco caolho'
+        },
+        'livro3': {
+            quantidadePaginas: 2000,
+            autor: 'Vader escritor',
+            editora: 'Imperium'
+        }
+    }
+     
+    return nomeLivro ? obj[nomeLivro]: obj;
+}
+
+/*
+Usando a função criada acima, imprima o objeto com todos os livros.
+*/
+ book();
+
+/*
+{ 
+  
+  livro1:
+   { quantidadePaginas: 400,
+     autor: 'José escritor',
+     editora: 'record' },
+  livro2:
+   { quantidadePaginas: 100,
+     autor: 'Manoel escritor',
+     editora: 'macaco caolho' },
+  livro3:
+   { quantidadePaginas: 2000,
+     autor: 'Vader escritor',
+     editora: 'Imperium' }
+    
+ } */
+
+/*
+Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
+usando a frase:
+"O livro [NOME_DO_LIVRO] tem [X] páginas!"
+*/
+
+console.log("O livro livro1 tem "+book('livro1').quantidadePaginas+" páginas!");
+
+/*
+Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
+a frase:
+"O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
+*/
+console.log("O autor do livro livro3 é "+book('livro3').autor+".");
+
+/*
+Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
+a frase:
+"O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
+*/
+console.log("O livro livro3 foi publicado pela editora "+book('livro3').editora+".");
---
+++ challenge-11/challenge-11.js
@@ -0,0 +1,96 @@
+(function(){
+  /*
+  Crie uma variável chamada `once`, que recebe `false` como seu valor.
+  Crie um loop que execute enquanto essa variável for verdadeira. Dentro do
+  loop, mostre no console a mensagem:
+  'Entrou ao menos uma vez!'
+  Qual loop você deve usar para que essa mensagem seja mostrada no console?
+  */
+  var once = false;
+  do {
+    console.log('Entrou ao menos uma vez!');
+  } while (once);
+
+  /*
+  Crie um objeto chamado `person`, que receba as seguintes propriedades:
+  - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
+  para o nome, idade, peso e data de nascimento dessa pessoa.
+  */
+  person = {
+    name: 'Emerson',
+    age: 23,
+    weight: 68,
+    birthday: '08 agosto'
+  }
+
+  /*
+  Use um loop para percorrer o objeto criado acima, mostrando no console
+  a frase:
+  'The [PROPERTY] of person is [VALUE]'
+  Aproveite e crie uma variável `counter` que vai contar quantas propriedades
+  esse objeto tem.
+  Após o loop, mostre a frase:
+  'The person has [COUNTER] properties'
+  */
+  for(value in person) {
+    console.log('the '+value+' is '+person[value]);
+  }
+
+  /*
+  Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
+  criado acima) é mais velha que a idade passada por parâmetro.
+  Se verdadeiro, retornar `true`. Senão, retornar false.
+  Após a função, mostrar a mensagem no console:
+  'The person has more than 25 years old? [TRUE/FALSE]'
+  */
+  function moreThan(age) {
+    return person.age > age;
+  }
+
+  console.log('The person has more than 25 years old? '+moreThan(30));
+
+  /*
+  Faça um loop de 0 a 20, que adicione cada número como um item de um
+  array chamado `numbers`. Se o contador for maior que 10, saia do loop.
+  Mostre no console os números no array.
+  */
+  console.log( 'De 0 a 10:' );
+
+  var i = 0;
+  var numbers = [];
+
+  while(i <= 20) {
+    if(i > 10) {
+      break;
+    }
+    numbers.push(i);
+    i++;
+  }
+
+  for(value in numbers) {
+    console.log(numbers[value]);
+  }
+
+  /*
+  Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
+  criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
+  esses. Se o número for ímpar, pular para o próximo número.
+  Mostrar no console os números do array.
+  */
+  console.log( 'Pares de 0 a 20:' );
+
+  i = 0;
+  numbers = [];
+
+  while(i <= 20) {
+    if(i % 2 > 0) {
+      i++;
+    }
+    numbers.push(i);
+    i++;
+  }
+
+  for(value in numbers) {
+    console.log(numbers[value]);
+  }
+})();
